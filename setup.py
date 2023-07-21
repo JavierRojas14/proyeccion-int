d65958d419db210de18cc462e4053ad80ae69a54
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-07-21 09:09:46 -0400

    Inicializa el repositorio

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the repository to analyze the projection and measure how big the new INT is gonna be',
+    author='Javier Rojas',
+    license='MIT',
+)
