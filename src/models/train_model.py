86f19c1c9ddef217d2e6c104ca2f3f4bf6393b89
Javier Rojas <javierrojas14.jr@gmail.com>
2023-11-10 12:02:34 -0300

    Entrena modelos con validacion cruzada

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,51 +1,71 @@
 from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
+import numpy as np
 
 
 class ModeloHibrido:
-    def __init__(self, modelo_1, modelo_2, n_splits=5):
+    def __init__(self, modelo_1, modelo_2, param_grid_1, param_grid_2, n_splits=5):
         self.modelo_1 = modelo_1
         self.modelo_2 = modelo_2
+        self.param_grid_1 = param_grid_1
+        self.param_grid_2 = param_grid_2
         self.best_params_1 = None
         self.best_params_2 = None
         self.n_splits = n_splits
+        self.cv_results_1 = None
+        self.cv_results_2 = None
 
-    def hyperparameter_tuning(self, param_grid_1, param_grid_2, X_1, X_2, y):
+    def hyperparameter_tuning(self, X_1, X_2, y):
         # Hyperparameter tuning for modelo_1
+        print("Tuning modelo_1...")
         grid_search_1 = GridSearchCV(
             self.modelo_1,
-            param_grid_1,
-            cv=TimeSeriesSplit(n_splits=self.n_splits, test_size=365),
+            self.param_grid_1,
+            cv=TimeSeriesSplit(n_splits=self.n_splits),
             scoring="neg_mean_squared_error",
+            return_train_score=True,
+            refit=True,
         )
         grid_search_1.fit(X_1, y)
         self.modelo_1 = grid_search_1.best_estimator_
         self.best_params_1 = grid_search_1.best_params_
+        self.cv_results_1 = grid_search_1.cv_results_
+
+        print("Best hyperparameters for modelo_1:", self.best_params_1)
+        print("CV Results for modelo_1:")
+        print("  Mean Train Score:", np.mean(self.cv_results_1["mean_train_score"]))
+        print("  Mean Test Score:", np.mean(self.cv_results_1["mean_test_score"]))
+        print("")
 
         # Predict residuals
         y_predict_1 = self.modelo_1.predict(X_1)
         y_resid = y - y_predict_1
 
         # Hyperparameter tuning for modelo_2
+        print("Tuning modelo_2...")
         grid_search_2 = GridSearchCV(
             self.modelo_2,
-            param_grid_2,
-            cv=TimeSeriesSplit(n_splits=self.n_splits, test_size=365),
+            self.param_grid_2,
+            cv=TimeSeriesSplit(n_splits=self.n_splits),
             scoring="neg_mean_squared_error",
+            return_train_score=True,
+            refit=True,
         )
         grid_search_2.fit(X_2, y_resid)
         self.modelo_2 = grid_search_2.best_estimator_
         self.best_params_2 = grid_search_2.best_params_
+        self.cv_results_2 = grid_search_2.cv_results_
+
+        print("Best hyperparameters for modelo_2:", self.best_params_2)
+        print("CV Results for modelo_2:")
+        print("  Mean Train Score:", np.mean(self.cv_results_2["mean_train_score"]))
+        print("  Mean Test Score:", np.mean(self.cv_results_2["mean_test_score"]))
+        print("")
 
     def fit(self, X_1, X_2, y):
-        # If hyperparameter tuning is not done, you can do it here or beforehand
-        # ...
+        # Perform hyperparameter tuning
+        self.hyperparameter_tuning(X_1, X_2, y)
 
-        self.modelo_1.fit(X_1, y)
-
-        y_predict_1 = self.modelo_1.predict(X_1)
-        y_resid = y - y_predict_1
-
-        self.modelo_2.fit(X_2, y_resid)
+        print("Training process completed.")
 
     def predict(self, X_1, X_2):
         y_predict_1 = self.modelo_1.predict(X_1)
