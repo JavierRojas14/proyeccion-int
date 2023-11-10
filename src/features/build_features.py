79ce785efbe84d89fda984acf81ea2860e2efcaa
Javier Rojas <javierrojas14.jr@gmail.com>
2023-11-10 10:18:29 -0300

    Predice con lag features nuevas

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -32,6 +32,35 @@ def add_lag_features(df, var_a_traer_valor):
     return df
 
 
+def create_lag_features(df, column_name, lag_values, fill_value=None):
+    """
+    Create lag features in a DataFrame for a specific column.
+
+    Parameters:
+    - df: DataFrame
+        The input DataFrame.
+    - column_name: str
+        The name of the column for which lag features will be created.
+    - lag_values: list of ints
+        A list of lag values.
+    - fill_value: int or None, default=None
+        The value to fill NaNs in lag features. If None, NaNs in lag features are left as NaNs.
+
+    Returns:
+    - new_df: DataFrame
+        A new DataFrame with lag features and filled NaNs in lag features.
+    """
+    new_df = df.copy()
+
+    for lag in lag_values:
+        new_column = f"{column_name}_lag_{lag}"
+        new_df[new_column] = new_df[column_name].shift(lag)
+        if fill_value is not None:
+            new_df[new_column] = new_df[new_column].fillna(fill_value)
+
+    return new_df
+
+
 def es_feriado(fecha):
     if FERIADOS_CHILE.get(fecha):
         return 1
