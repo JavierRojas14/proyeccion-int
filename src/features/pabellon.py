1e301b0ea66cd4d90ad0a59074cb0dbe1d8e64b1
Javier Rojas <javierrojas14.jr@gmail.com>
2024-08-02 15:49:54 -0400

    Obtiene porcentaje de complicaciones

--- src/features/pabellon.py
+++ src/features/pabellon.py
@@ -189,3 +189,73 @@ def calcular_cantidad_de_pabellones_necesarios(tiempo_utilizado_pabellon_horas, 
     print(tabulate(cantidad_de_pabellones_necesarios.head(), headers="keys", tablefmt="pretty"))
     print()
     return cantidad_de_pabellones_necesarios
+
+
+def obtener_resumen_ocurrencia_complicacion(df, df_filtrada):
+    # Obtiene el resumen de la cantidad de ocurrencias del DataFrame total y el filtrado
+    resumen = pd.DataFrame(
+        {
+            "totales": df.groupby(["ano_de_intervencion", "especialidad"]).size(),
+            "ocurrencia_filtrado": df_filtrada.groupby(
+                ["ano_de_intervencion", "especialidad"]
+            ).size(),
+        }
+    )
+
+    # Obtiene el resumen acumulado en el periodo
+    resumen_acumulado = resumen.sum()
+
+    # Obtiene los porcentajes de ocurrencia desglosados y acumulados
+    resumen["fraccion"] = resumen["ocurrencia_filtrado"] / resumen["totales"]
+    porcentaje_acumulado = resumen_acumulado["ocurrencia_filtrado"] / resumen_acumulado["totales"]
+
+    # Obtiene el resumen por especialidad acumulado
+    resumen_acumulado_por_especialidad = (
+        resumen.reset_index().groupby("especialidad")[["totales", "ocurrencia_filtrado"]].sum()
+    )
+    resumen_acumulado_por_especialidad["fraccion"] = (
+        resumen_acumulado_por_especialidad["ocurrencia_filtrado"]
+        / resumen_acumulado_por_especialidad["totales"]
+    )
+
+    return resumen, resumen_acumulado, resumen_acumulado_por_especialidad, porcentaje_acumulado
+
+
+def buscar_nombre_operacion_pabellon(df, operaciones):
+    # Filtra la base de datos segun el nombre de la operacion
+    return df[df["nombre_de_la_operacion"].fillna("").str.contains(operaciones, regex=True)]
+
+
+def buscar_nombre_diagnosticos_pabellon(df, diagnosticos):
+    # Filtra la base de datos segun el nombre del diagnostico 1 y 2
+    return df[
+        (df["primer_diagnostico"].fillna("").str.contains(diagnosticos, regex=True))
+        | (df["segundo_diagnostico"].fillna("").str.contains(diagnosticos, regex=True))
+    ]
+
+
+def iterar_en_complicaciones_a_buscar(df, dict_textos_a_buscar, tipo_complicacion):
+    # Decide que parametro a buscar en la base de datos
+    busqueda_a_realizar = {
+        "intervencion_quirurgica": buscar_nombre_operacion_pabellon,
+        "diagnostico": buscar_nombre_diagnosticos_pabellon,
+    }
+    funcion_a_ocupar_para_buscar = busqueda_a_realizar[tipo_complicacion]
+
+    # Itera por el diccionario de busqueda y guarda los resultados
+    resultados = {}
+    df_resultado = pd.DataFrame()
+    for nombre_complicacion, textos_a_buscar in dict_textos_a_buscar.items():
+        df_filtrada = funcion_a_ocupar_para_buscar(df, textos_a_buscar)
+        resumen_filtrado = obtener_resumen_ocurrencia_complicacion(df, df_filtrada)
+        resultados[nombre_complicacion] = resumen_filtrado
+
+        # print(f"> {nombre_complicacion}: {resumen_filtrado[2]}")
+
+        # Concatena resultados acumulados en el periodo por complicacion
+        resultado_acumulado = resumen_filtrado[2].reset_index()
+        resultado_acumulado["complicacion"] = nombre_complicacion
+        resultado_acumulado["texto_a_buscar"] = textos_a_buscar
+        df_resultado = pd.concat([df_resultado, resultado_acumulado])
+
+    return resultados, df_resultado
